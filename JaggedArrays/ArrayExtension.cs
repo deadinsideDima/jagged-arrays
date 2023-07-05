b397e27ca2048e6d2260d4e99e48e4f2e3c49700
Dmitry Nesterenko <trapwhiteclown@gmail.com>
2023-07-05 14:12:41 +0300

    Jag!

--- JaggedArrays/ArrayExtension.cs
+++ JaggedArrays/ArrayExtension.cs
@@ -4,6 +4,22 @@ namespace JaggedArrays
 {
     public static class ArrayExtension
     {
+        public static int Sum(int[]? arr)
+        {
+            if (arr == null)
+            {
+                return 0;
+            }
+
+            int ans = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                ans += arr[i];
+            }
+
+            return ans;
+        }
+
         /// <summary>
         /// Orders the rows in a jagged-array by ascending of the sum of the elements in them.
         /// </summary>
@@ -11,7 +27,23 @@ namespace JaggedArrays
         /// <exception cref="ArgumentNullException">Thrown when source in null.</exception>
         public static void OrderByAscendingBySum(this int[][]? source)
         {
-            throw new NotImplementedException();
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            for (int i = 0; i < source.Length - 1; i++)
+            {
+                for (int j = 0; j < source.Length - 1; j++)
+                {
+                    if (Sum(source[j]) > Sum(source[j + 1]))
+                    {
+                        var temp = source[j];
+                        source[j] = source[j + 1];
+                        source[j + 1] = temp;
+                    }
+                }
+            }
         }
         
         /// <summary>
@@ -21,9 +53,44 @@ namespace JaggedArrays
         /// <exception cref="ArgumentNullException">Thrown when source in null.</exception>
         public static void OrderByDescendingBySum(this int[][]? source)
         {
-            throw new NotImplementedException();
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            for (int i = 0; i < source.Length - 1; i++)
+            {
+                for (int j = 0; j < source.Length - 1; j++)
+                {
+                    if (Sum(source[j]) < Sum(source[j + 1]))
+                    {
+                        var temp = source[j];
+                        source[j] = source[j + 1];
+                        source[j + 1] = temp;
+                    }
+                }
+            }
         }
-        
+
+        public static int Max(int[]? arr)
+        {
+            if (arr == null)
+            {
+                return int.MinValue;
+            }
+
+            int temp = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (temp < arr[i])
+                {
+                    temp = arr[i];
+                }
+            }
+
+            return temp;
+        }
+
         /// <summary>
         /// Orders the rows in a jagged-array by ascending of the max of the elements in them.
         /// </summary>
@@ -31,7 +98,23 @@ namespace JaggedArrays
         /// <exception cref="ArgumentNullException">Thrown when source in null.</exception>
         public static void OrderByAscendingByMax(this int[][]? source)
         {
-            throw new NotImplementedException();
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            for (int i = 0; i < source.Length - 1; i++)
+            {
+                for (int j = 0; j < source.Length - 1; j++)
+                {
+                    if (Max(source[j]) > Max(source[j + 1]))
+                    {
+                        var temp = source[j];
+                        source[j] = source[j + 1];
+                        source[j + 1] = temp;
+                    }
+                }
+            }
         }
         
         /// <summary>
@@ -41,7 +124,23 @@ namespace JaggedArrays
         /// <exception cref="ArgumentNullException">Thrown when source in null.</exception>
         public static void OrderByDescendingByMax(this int[][]? source)
         {
-            throw new NotImplementedException();
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            for (int i = 0; i < source.Length - 1; i++)
+            {
+                for (int j = 0; j < source.Length - 1; j++)
+                {
+                    if (Max(source[j]) < Max(source[j + 1]))
+                    {
+                        var temp = source[j];
+                        source[j] = source[j + 1];
+                        source[j + 1] = temp;
+                    }
+                }
+            }
         }
     }
 }
